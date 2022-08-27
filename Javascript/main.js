f044e34e95ac3f96ef46074ffa151c5421a54e07
yusiifi <109171963+yusiifi@users.noreply.github.com>
2022-08-27 12:37:31 +0400

    Add files via upload

---
+++ Javascript/main.js
@@ -0,0 +1,76 @@
+/*
+
+function showconsole(arr) {
+    i=0
+    while (i<arr.length) {
+        console.log(arr[i]);
+        i+=1
+        
+    }
+    
+}
+var arr=["cup","plate","spoon","knife","fork"]
+console.log(showconsole(arr));
+*/
+
+/*
+function sherfi(arr) {
+    for(var i=0;i<=arr.length-1;i++){
+        if (arr[i].startsWith("s")) {
+            console.log(arr[i]);
+
+            
+        }
+    }
+
+    
+}
+
+var arr=["xiyar","pomidor","sogan","yerkoku","sarimsaq","semeni"]
+console.log(sherfi(arr));
+*/
+
+/*
+function sumofnumbers(a,b) {
+    var sum=0
+    for(var i=a;i<=b;i++){
+        sum+=i
+    }
+    return sum
+    
+}
+
+var a=1
+var b=100
+console.log(sumofnumbers(a,b));
+*/
+/*
+function show5letters(arr){
+    i=0
+    while (i<arr.length) {
+        if (arr[i].length=5) {
+            console.log((arr[i].charAt(arr[i].length-1)));
+            
+            
+        }else{
+            continue
+        }
+        i+=1
+        
+    }
+}
+
+var arr=["kitab","defter","qelem","karandas","jurnal","pozan","vereq"]
+console.log(show5letters(arr));
+*/
+/*
+for(var i=100000;i>0;i--){
+    if(i%9999==0){
+        console.log(i);
+        break
+    }
+    else{
+        continue
+    }
+}
+*/
